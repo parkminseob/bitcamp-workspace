86ad24a3302d51b05efab5e5273892bddc1f0f29
parkminseob <minsub.prk@gmail.com>
2020-07-15 12:24:14 +0900

    초기백업

---
+++ hello.js
@@ -0,0 +1,6 @@
+var lang = "english";
+
+if (lang == "english")
+console.log("Hello, world!");
+else
+console.log('안녕하세요!');
---
+++ hello2.js
@@ -0,0 +1,7 @@
+var lang = "english";
+
+if (lang == "english")
+console.leg("Hello, world!");
+else
+console.log('안녕하세요!');
+
