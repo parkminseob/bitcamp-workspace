86b84f49ab1a76a72cab29c23b3517d00c590e60
parkminseob <minsub.prk@gmail.com>
2020-07-22 14:18:36 +0900

    7/22

--- bitcamp-java-basic/src/main/java/com/eomcs/basic/Hello2.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/Hello2.java
@@ -4,7 +4,7 @@ class Hello2 {
     public static void main (String[] args){
         
         System.out.println("com/eomcs/basic/Hello2.java");
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!2");
     }
 }
 
--- bitcamp-java-basic/src/main/java/com/eomcs/basic/ex03/Exam0340.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex03/Exam0340.java
@@ -4,24 +4,30 @@ package com.eomcs.basic.ex03;
 
 class Exam0340 { 
    public static void main(String[] args)   {     
-        // 4바이트 메모리에 부동소수점 저장하기  
+        
+     
+        // 4바이트 메모리에 부동소수점 저장하기 
+        System.out.println("1---------------------");
         System.out.println(999.9999f);
         System.out.println(999999.9f); 
         System.out.println(9.999999f);
 
-              
+        
         // 4바이트 메모리는 최대 7자리 부동 소수점을 저장할 수 있다.
         //초과하는 경우 반올림 처리되거나 잘린다.
+        System.out.println("2---------------------");
         System.out.println(999.99994f);
         System.out.println(9999999.4f); 
         System.out.println(9.9999994f);
 
         //메모리 크기를 넘는 값은 반올림 처리되어 저장된다.  
+        System.out.println("3---------------------");
         System.out.println(987654321.1234567f);
         System.out.println(987654321123456.7f); 
         System.out.println(9.876543211234567f);
 
         //8바이트 메모리에 부동소수점 저장하기
+        System.out.println("4---------------------");
         System.out.println(987654321.1234567);
         System.out.println(987654321123456.7); 
         System.out.println(9.876543211234567);
@@ -29,6 +35,7 @@ class Exam0340 {
         // 8바이트 메모리에는 최대 16자리의 부동 소수점을
         // 저장할 수 있다. 
         // 초과하면 반올림 처리되거나 잘린다.
+        System.out.println("5---------------------");
         System.out.println(987654321.12345678);
         System.out.println(9876543211234567.8); 
         System.out.println(9.8765432112345678);
@@ -45,7 +52,8 @@ class Exam0340 {
        
 
        //부동소수점의 일부는 정확하게 2진수로 변환할 수 없다.
-       System.out.println(7 * 0.1); //예상값 = 0.7
+        System.out.print("6---------------------");
+        System.out.println(7 * 0.1); //예상값 = 0.7
    }
     }
 
