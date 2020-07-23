30817188eac880037894e86ea58fc2c81f1db853
parkminseob <minsub.prk@gmail.com>
2020-07-23 10:50:06 +0900

    m

--- bitcamp-java-basic/src/main/java/com/eomcs/basic/ex04/Exam0110.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex04/Exam0110.java
@@ -1,7 +1,11 @@
-// 변수 선
+// 변수 선언
+// - 데이터를 저장할 메모리를 확보하는 명령
+// 
 package com.eomcs.basic.ex04;
 
 public class Exam0110 {
-  
+  public static void main(String[] args) { //
+    int i;
+  }
 
 }
