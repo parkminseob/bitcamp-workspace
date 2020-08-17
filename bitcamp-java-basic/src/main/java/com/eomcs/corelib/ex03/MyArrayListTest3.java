2e0b8322ef68f0cbf1685617f6dbb17b0cc98872
parkminseob <minsub.prk@gmail.com>
2020-08-17 10:35:38 +0900

    8/17

--- bitcamp-java-basic/src/main/java/com/eomcs/corelib/ex03/MyArrayListTest3.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/corelib/ex03/MyArrayListTest3.java
@@ -4,24 +4,29 @@ public class MyArrayListTest3 {
   public static void main(String[] args) {
 
     MyArrayList list1 = new MyArrayList();
-
-    //aaa, bbb, ccc, ddd, eee
     list1.add("aaa");
     list1.add("bbb");
     list1.add("ccc");
     list1.add("ddd");
     list1.add("eee");
+    list1.add("fff");
+    list1.add("ggg");
 
+    list1.remove(2); // aaa, bbb, ddd, eee, fff, ggg
+    list1.remove(0); // bbb, ddd, eee, fff, ggg
+    list1.remove(4); // bbb, ddd, eee, fff
+    print(list1);
 
-    MyArrayList list2 = new MyArrayList();
-    //fff, ggg, hhh, iii
+    System.out.println("----------------");
+
+    MyArrayList list2 = new MyArrayList(10);
     list2.add("fff");
     list2.add("ggg");
     list2.add("hhh");
     list2.add("iii");
+    list2.add("kkk");
+    list2.add("lll");
 
-    print(list1);
-    System.out.println("----------------");
     print(list2);
 
   }
--- bitcamp-java-basic/src/main/java/com/eomcs/corelib/ex03/MyArrayListTest4.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/corelib/ex03/MyArrayListTest4.java
@@ -5,26 +5,30 @@ import java.util.ArrayList;
 public class MyArrayListTest4 {
   public static void main(String[] args) {
 
-    ArrayList list1 = new ArrayList();
-
-    //aaa, bbb, ccc, ddd, eee
+    ArrayList list1 = new ArrayList(100);
     list1.add("aaa");
     list1.add("bbb");
     list1.add("ccc");
     list1.add("ddd");
     list1.add("eee");
+    list1.add("fff");
+    list1.add("ggg");
 
+    print(list1);
+
+    System.out.println("----------------");
 
     ArrayList list2 = new ArrayList();
-    //fff, ggg, hhh, iii
     list2.add("fff");
     list2.add("ggg");
     list2.add("hhh");
     list2.add("iii");
+    list2.add("jjj");
+    list2.add("kkk");
+    list2.add("lll");
 
-    print(list1);
-    System.out.println("----------------");
     print(list2);
+
 
   }
 
