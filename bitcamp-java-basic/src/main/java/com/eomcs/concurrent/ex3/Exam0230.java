3533433cf6ff90589d81cdba2ec103151c039350
parkminseob <minsub.prk@gmail.com>
2020-10-11 17:07:31 +0900

    10/11공부

--- bitcamp-java-basic/src/main/java/com/eomcs/concurrent/ex3/Exam0230.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/concurrent/ex3/Exam0230.java
@@ -2,12 +2,11 @@ package com.eomcs.concurrent.ex3;
 public class Exam0230 {
   public static void main(String[] args) {
 
-    Thread t = new Thread(() -> {
+    new Thread(() -> {
       for(int i = 0; i < 1000; i++) {
         System.out.println("===> "+ i);
       }
-    });
-    t.start();
+    }).start();
 
     // "main" 스레드는 Thread와 상관없이 병행하여 실행한다.
     for(int i = 0; i < 1000; i++) {
